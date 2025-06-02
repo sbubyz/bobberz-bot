04373425296afcc5f4725125deed208208ad233a
sbubyz <roberto.belcampo@gmail.com>
2025-06-02 20:34:22 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  author: '‎bobt',
+  botName: "bobberz bot",
+  botOwner: 'Bobby', 
+  ownerNumber: '393757199048', 
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
